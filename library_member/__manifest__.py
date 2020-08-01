64c90d425fc43dae926d6f5b857ff0830f2cffb9
Daniel Reis <dreis@opensourceintegrators.com>
2020-08-01 21:28:48 +0100

    Ch04

---
+++ library_member/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Library Members",
+    "description": "Manage members borrowing books.",
+    "author": "Daniel Reis",
+    "depends": ["library_app", "mail"],
+    "application": False,
+    "data": [
+        "security/library_security.xml",
+        "security/ir.model.access.csv",
+        "views/book_view.xml",
+        "views/member_view.xml",
+        "views/library_menu.xml",
+        "views/book_list_template.xml",
+    ],
+}
