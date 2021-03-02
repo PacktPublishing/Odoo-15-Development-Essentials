7cc0b8bfe44d86f80a259e2ed4042acc1bcc1bb8
Daniel Reis <dreis@opensourceintegrators.com>
2021-03-02 00:45:03 +0000

    Ch11

--- library_checkout/models/library_checkout_line.py
+++ library_checkout/models/library_checkout_line.py
@@ -8,3 +8,4 @@ class CheckoutLine(models.Model):
     checkout_id = fields.Many2one("library.checkout", required=True)
     book_id = fields.Many2one("library.book", required=True)
     note = fields.Char("Notes")
+    book_cover = fields.Binary(related="book_id.image")
