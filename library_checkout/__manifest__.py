b4bc8e6a88b122bd9d0635759fd63da73d5747fa
Daniel Reis <dreis@opensourceintegrators.com>
2021-11-07 18:52:08 +0000

    ch11-v15

--- library_checkout/__manifest__.py
+++ library_checkout/__manifest__.py
@@ -9,6 +9,14 @@
         "wizard/checkout_mass_message_wizard_view.xml",
         "views/library_menu.xml",
         "views/checkout_view.xml",
+        "views/checkout_kanban_view.xml",  # Ch11
         "data/stage_data.xml",
+        # "views/assets.xml",  # Ch11, until Oddo14
     ],
+    "assets": {  # Ch11, since Odoo 15
+        "web.assets_backend": {
+            "library_checkout/static/src/css/checkout.css",
+            "library_checkout/static/src/js/checkout.js",
+        }
+    }
 }
